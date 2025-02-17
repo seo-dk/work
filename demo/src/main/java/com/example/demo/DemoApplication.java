88ce7674bfa6f8cbaa66881d273e0ce902b04e36
seo-kd <tab006@naver.com>
2025-02-17 16:14:01 +0900

    demo App

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -17,7 +17,7 @@ public class DemoApplication {
     public CommandLineRunner run(MyEntityRepository repository) {
         return args -> {
             // 데이터베이스 연결 테스트 (MySQL에 데이터를 저장하거나 조회해볼 수 있음)
-            System.out.println("MySQL 데이터베이스에 연결되었습니다!");
+            System.out.println("MySQL 데이터베이스에 연결되었습니다");
             // 예시로 데이터를 저장
             MyEntity entity = new MyEntity("team8-edu-Data");
             entity.setId(System.currentTimeMillis());
