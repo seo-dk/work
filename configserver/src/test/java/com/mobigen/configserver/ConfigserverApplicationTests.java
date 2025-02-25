a7f5a08ee94bbb0e047e0ea19537d597755e86e6
master <hankgood958@gmail.com>
2025-02-25 18:13:22 +0900

    ConfigServer 생성

---
+++ configserver/src/test/java/com/mobigen/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobigen.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
