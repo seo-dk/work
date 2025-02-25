60254c4b712ec3fab4e5ff1ef5cddeeda04c460e
master <hankgood958@gmail.com>
2025-02-25 21:43:29 +0900

    없는 어노테이션 삭제

--- gatewayserver/src/main/java/com/mobigen/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/mobigen/gatewayserver/GatewayserverApplication.java
@@ -2,10 +2,8 @@ package com.mobigen.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class GatewayserverApplication {
 
 	public static void main(String[] args) {
