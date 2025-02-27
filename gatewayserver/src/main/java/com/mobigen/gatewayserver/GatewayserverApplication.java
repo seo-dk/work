e8c64bf7dbc05d2df521d595c424da6f22f8b627
seo-dk <tab006@naver.com>
2025-02-27 17:31:51 +0900

    msa

--- gatewayserver/src/main/java/com/mobigen/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/mobigen/gatewayserver/GatewayserverApplication.java
@@ -2,9 +2,10 @@ package com.mobigen.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class GatewayserverApplication {
 
 	public static void main(String[] args) {
