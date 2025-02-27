c80e70c01720651cddabbc25c2f1d8106cf9705d
seo-dk <tab006@naver.com>
2025-02-27 19:17:03 +0900

    msa

--- accounts/src/main/java/com/eazybytes/accounts/dto/ErrorResponseDto.java
+++ accounts/src/main/java/com/eazybytes/accounts/dto/ErrorResponseDto.java
@@ -12,7 +12,12 @@ import java.time.LocalDateTime;
         name = "ErrorResponse",
         description = "Schema to hold error response information"
 )
+
+
+
 public class ErrorResponseDto {
+
+    public ErrorResponseDto() {}
 
     @Schema(
             description = "API path invoked by client"
