3e9b7478a0b742fb3f6bbc987b12af464d9cdca4
seo-dk <tab006@naver.com>
2025-02-28 10:58:35 +0900

    msa

--- gatewayserver/src/main/java/com/mobigen/gatewayserver/controller/FallbackController.java
+++ gatewayserver/src/main/java/com/mobigen/gatewayserver/controller/FallbackController.java
@@ -9,7 +9,7 @@ public class FallbackController {
 
     @RequestMapping("/contactSupport")
     public Mono<String> contactSupport() {
-        return Mono.just("An error occurred. Please try after some time or contact support team!!!");
+        return Mono.just("Account Service is currently unavailable. Please try again later.");
     }
 
 }
