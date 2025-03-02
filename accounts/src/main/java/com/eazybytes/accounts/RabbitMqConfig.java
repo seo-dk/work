c51ec2b097a3895d2da5a5b796e0ee142361b416
seo-kd <tab006@naver.com>
2025-03-03 01:13:46 +0900

    msa

--- accounts/src/main/java/com/eazybytes/accounts/RabbitMqConfig.java
+++ accounts/src/main/java/com/eazybytes/accounts/RabbitMqConfig.java
@@ -4,8 +4,10 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 
 @Configuration
 public class RabbitMqConfig {
@@ -14,6 +16,15 @@ public class RabbitMqConfig {
     public static final String QUEUE_NAME = "account-service-queue";
     public static final String EXCHANGE_NAME = "account-service-exchange";
     public static final String ROUTING_KEY = "account-service-routing-key";
+
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setAutoStartup(true);  // 🔹 Bean이 안전하게 시작됨
+        return factory;
+    }
 
     // Queue 생성
     @Bean
