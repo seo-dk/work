59e72e181968558846bf3d0fb3cb7217591e8fbe
seo-dk <tab006@naver.com>
2025-03-04 11:49:21 +0900

    msa

--- accounts/src/main/java/com/eazybytes/accounts/service/impl/AccountsMergeImpl.java
+++ accounts/src/main/java/com/eazybytes/accounts/service/impl/AccountsMergeImpl.java
@@ -20,6 +20,7 @@ import com.eazybytes.accounts.service.client.LoansFeignClient;
 import com.eazybytes.accounts.repository.CustomerRepository;
 import com.eazybytes.accounts.repository.AccountsRepository;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -31,6 +32,7 @@ public class AccountsMergeImpl implements IAccountsMerge{
     private final CardsFeignClient cardsFeignClient;
     private final LoansFeignClient loansFeignClient;
     private final RabbitMqProducer rabbitMQProducer;
+    private final CircuitBreakerRegistry circuitBreakerRegistry;
 
     @Override
     @CircuitBreaker(name = "accountServiceFallback", fallbackMethod = "fallbackService")
@@ -64,11 +66,18 @@ public class AccountsMergeImpl implements IAccountsMerge{
     //서킷 브레이커 Fallback 메서드
     public MergeDto fallbackService(String mobileNumber, Throwable t) {
 
+        // 현재 서킷 브레이커 상태 확인
+        io.github.resilience4j.circuitbreaker.CircuitBreaker circuitBreaker = circuitBreakerRegistry.circuitBreaker("accountServiceFallback");
+        io.github.resilience4j.circuitbreaker. CircuitBreaker.State state = circuitBreaker.getState();
+
         String errorMessage = String.format(" [Circuit Breaker open] 고객(%s)의 카드 또는 대출 정보를 가져오지 못했습니다. 원인: %s",
         mobileNumber, t.getMessage());
         rabbitMQProducer.sendMessage(errorMessage);
 
-        throw new CustomCircuitBreakerException("서킷 브레이커가 OPEN 상태입니다. 요청을 처리할 수 없습니다.", t);
+        if (state==io.github.resilience4j.circuitbreaker.CircuitBreaker.State.OPEN) {
+            throw new CustomCircuitBreakerException("서킷 브레이커가 OPEN 상태입니다. 요청을 처리할 수 없습니다.", t);
+        }
+        throw new RuntimeException("Account service error");
             
         // CustomerDto customerDto = new CustomerDto();
         // customerDto.setName("서비스 이용 불가");
