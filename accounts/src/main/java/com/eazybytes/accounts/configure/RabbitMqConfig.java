0802b53f3fa3039b81823211edb38825c355c70b
seo-dk <tab006@naver.com>
2025-03-07 17:08:26 +0900

    msa

--- accounts/src/main/java/com/eazybytes/accounts/configure/RabbitMqConfig.java
+++ accounts/src/main/java/com/eazybytes/accounts/configure/RabbitMqConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 @EnableRabbit
@@ -19,16 +20,28 @@ public class RabbitMqConfig {
     public static final String ROUTING_KEY = "account-service-routing-key";
     public static final String QUEUE_NAME = "account-service-queue";
 
-    @Bean
-    public CachingConnectionFactory connectionFactory() {
-        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
-        connectionFactory.setHost("192.168.100.221");  // 직접 IP 사용
-        connectionFactory.setPort(30802);             // NodePort 사용
-        connectionFactory.setUsername("guest");
-        connectionFactory.setPassword("guest");
-        connectionFactory.setVirtualHost("/");
-        return connectionFactory;
-    }
+    // @Bean
+    // public CachingConnectionFactory connectionFactory() {
+    //     CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+    //     connectionFactory.setHost("192.168.100.221");  // 직접 IP 사용
+    //     connectionFactory.setPort(30802);             // NodePort 사용
+    //     connectionFactory.setUsername("guest");
+    //     connectionFactory.setPassword("guest");
+    //     connectionFactory.setVirtualHost("/");
+    //     return connectionFactory;
+    // }
+
+    @Value("${spring.rabbitmq.host}")
+    private String rabbitmqHost;
+  
+    @Value("${spring.rabbitmq.port}")
+    private int rabbitmqPort;
+  
+    @Value("${spring.rabbitmq.username}")
+    private String rabbitmqUsername;
+  
+    @Value("${spring.rabbitmq.password}")
+    private String rabbitmqPassword;
 
     @Bean
     Queue queue() {
