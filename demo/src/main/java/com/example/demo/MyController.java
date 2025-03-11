90874cb08bd0f9cf0fbd9745b1264f39aeb94a29
seo-dk <tab006@naver.com>
2025-03-11 14:52:14 +0900

    msa

--- demo/src/main/java/com/example/demo/MyController.java
+++ demo/src/main/java/com/example/demo/MyController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -16,11 +15,6 @@ public class MyController {
 
     @Autowired
     private MyEntityRepository repository;
-    //private final RestTemplate restTemplate;
-
-    // public MyController(RestTemplate restTemplate) {
-    //     this.restTemplate = restTemplate;
-    // }
 
     @GetMapping("/fetch")
     public List<MyEntity> getData() {
